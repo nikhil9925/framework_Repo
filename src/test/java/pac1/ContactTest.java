ccbe9d68510c61218db6c4509d98fad2e19af6dc
nikhil9925 <160498035+nikhil9925@users.noreply.github.com>
2025-07-11 11:39:29 +0530

    first push

---
+++ src/test/java/pac1/ContactTest.java
@@ -0,0 +1,26 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+ 
+	@Test
+	public void createContactTest() {
+		
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String UN=System.getProperty("username");
+		String PW=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(UN);
+		System.out.println(PW);
+		System.out.println("Execute createContactTest");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("Execute modifyContactTest");
+	}
+}
---
+++ src/test/java/pac2/OrgTest.java
@@ -0,0 +1,16 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class  OrgTest{
+ 
+	@Test
+	public void createOrgTest() {
+		System.out.println("Execute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("Execute modifyOrgTest");
+	}
+}
